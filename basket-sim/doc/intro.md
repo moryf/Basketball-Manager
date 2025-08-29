307c4496e404e960cef8492b61a30b682f4c1e1b
Moryf <aleksa.kb24@gmail.com>
2025-08-29 10:54:41 +0200

    First commit

---
+++ basket-sim/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to basket-sim
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ basket-sim/test/basket_sim/core_test.clj
@@ -0,0 +1,7 @@
+(ns basket-sim.core-test
+  (:require [clojure.test :refer :all]
+            [basket-sim.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
